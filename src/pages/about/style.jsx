cc30c33e63e3292c1ddb99d0ba523cb3ca583125
OgistDipen <ogistdipen@outlook.com>
2020-03-01 01:34:18 +0100

    pathsee

--- src/pages/about/style.jsx
+++ src/pages/about/style.jsx
@@ -3,6 +3,12 @@ import styled from "styled-components";
 const AboutStyle = styled.div`
   min-height: calc(100vh - 75px);
   position: relative;
+
+  @media only screen and (max-width: 460px) {
+    width: 100%;
+    height: 100%;
+    padding: 0px;
+  }
 `;
 
 export default AboutStyle;
--- src/pages/contact/style.jsx
+++ src/pages/contact/style.jsx
@@ -3,6 +3,12 @@ import styled from "styled-components";
 const ContactStyle = styled.div`
   background: #fbfcfd;
   height: calc(100vh - 75px);
+
+  @media only screen and (max-width: 460px) {
+    width: 100%;
+    height: 100%;
+    padding: 0px;
+  }
 `;
 
 export default ContactStyle;
