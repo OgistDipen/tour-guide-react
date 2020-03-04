d6191e41a06a15a0f438ba0ba8ef24462f547251
OgistDipen <ogistdipen@outlook.com>
2020-03-04 13:15:00 +0100

    browser Router Wraped Switch

--- src/switch/Main.jsx
+++ src/switch/Main.jsx
@@ -1,25 +1,31 @@
 import React, { Component } from "react";
-import { Switch, Route, Redirect } from "react-router";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import Home from "../pages/home";
 import About from "../pages/about";
 import Contact from "../pages/contact";
 import SingleTour from "../pages/tours/SingleTour";
 
 const DefaultSwitch = () => (
-  <Switch>
-    <Route exact path="/" component={Home} />
-    <Route exact path="/about" component={About} />
-    <Route exact path="/contact" component={Contact} />
-    <Route exact path="/tours/:tourName" component={SingleTour} />
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/contact" component={Contact} />
+      <Route exact path="/tours/:tourName" component={SingleTour} />
 
-    <Redirect to="/" />
-  </Switch>
+      <Redirect to="/" />
+    </Switch>
+  </BrowserRouter>
 );
 
 class Main extends Component {
   render() {
     let routes = <DefaultSwitch />;
-    return <Switch>{routes}</Switch>;
+    return (
+      <BrowserRouter>
+        <Switch>{routes}</Switch>
+      </BrowserRouter>
+    );
   }
 }
 
